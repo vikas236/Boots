6f5cb185c848f47dfa9e8b3991e2720ab22c8d2f
vikas236 <saivikas461@gmail.com>
2024-12-19 13:18:06 +0530

    update image source fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,10 +24,10 @@ function Home() {
     return (
       <div className="latest_designs carousel">
         <div className="carousel-images">
-          <img src="image1.jpg" alt="Image 1" />
-          <img src="image2.jpg" alt="Image 2" />
-          <img src="image3.jpg" alt="Image 3" />
-          <img src="image4.jpg" alt="Image 4" />
+          <img src="" alt="Image 1" />
+          <img src="" alt="Image 2" />
+          <img src="" alt="Image 3" />
+          <img src="" alt="Image 4" />
         </div>
       </div>
     );
